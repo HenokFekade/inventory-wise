2c147791fcfceda0f3919bc521e2e77eefe48044
henokfekade <henok.fekade2468@gmail.com>
2025-11-17 07:07:54 +0300

    currency crud is done

---
+++ core/dependencies/currency/update_currency_validator.py
@@ -0,0 +1,23 @@
+from uuid import UUID
+
+from fastapi import Depends, Path
+
+from apps.currency.repositories.currency import CurrencyRepository
+from apps.currency.schemas.currency import UpdateCurrencySchema
+from core.dependencies.currency.currency_repo import currency_repo_dep
+from exceptions.unprocessable_entity import UnprocessableEntityException
+
+
+async def update_currency_validator_dep(
+        data: UpdateCurrencySchema,
+        _id: UUID = Path(alias="id"),
+        repo: CurrencyRepository = Depends(currency_repo_dep),
+) -> UpdateCurrencySchema:
+    result = await repo.by_name(data.name)
+    if result and result.id == _id:
+        UnprocessableEntityException.throw("name", ["Name already taken."])
+
+    result = await repo.by_code(data.code)
+    if result and result.id == _id:
+        UnprocessableEntityException.throw("code", ["Code already taken."])
+    return data
